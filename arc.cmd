badde2d84b54dde1f4ac94e0c7a390c1c913e703
Viktoriia <Viktoriia_Sova@epam.com>
2015-04-17 10:28:14 +0300

    ProjectExample_f

--- arc.cmd
+++
@@ -1,4 +0,0 @@
-set PATH="C:\Program Files\7-Zip\"
-7z a -tzip %2\arc_%date%_%time:~0,2%.%time:~3,2%.zip "%1" 
-
-
--- task4.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-schtasks /create /sc MINUTE /mo 1 /tn "Task 4" /tr "%CD%\task41.cmd %1 %CD%"
